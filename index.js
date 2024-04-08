fab3a360671aeac5cc26106c886b37982741c28d
AgusDesi04 <agusdesi2004@gmail.com>
2024-04-08 11:56:46 -0300

    pre-entrega2 corregida

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 let res = "S"
-let cantidad = 0
-let suma = 0
+let cantidad = prompt('ingrese la cantidad de personas que desea analizar')
+let personas = []
+
+function cargarPersonas(cantidad, personas) {
+  nombre = prompt('ingrese el nombre de la persona')
+  genero = prompt('ingrese el genero de la persona(H/M)')
+  empleo = prompt('ingrese si la persona es empleada o desempleada (E/D)')
+  edad = prompt('ingrese la edad de la persona')
+
+  let persona = new Persona(nombre, genero, empleo, edad)
+  personas.push = persona
+}
+
+console.log(personas)
+
+
+/*function calcularPromedio(sumatoria, cantidad) {
+  let promedio = sumatoria / cantidad
+  return promedio
+}
+
+
+
+
+promedio = calcularPromedio(suma, cantidad)
+
+alert(promedio)
+
+
+const arrayEdades = []
 do {
   let edad = Number(prompt("Ingrese la edad de la persona:"));
   if (edad > 0) {
-    suma = suma + edad;
+    arrayEdades.push(edad);
     cantidad++
     res = prompt("Quieres seguir ingresando edades?:(S/N)")
+    console.log(arrayEdades)
   } else {
     alert("La edad teiene que ser mayor a 0")
   }
@@ -14,12 +43,9 @@ do {
 
 } while (res == "S")
 
-function calcularPromedio(sumatoria, cantidad) {
-  let promedio = sumatoria / cantidad
-  return promedio
+let suma = 0
+
+for (var i = 0; i < arrayEdades.length; i++) {
+  suma += arrayEdades[i]
 }
-
-
-promedio = calcularPromedio(suma, cantidad)
-
-alert(promedio)
+console.log(suma)*/
---
+++ personaClass.js
@@ -0,0 +1,8 @@
+class Persona {
+  constructor(sNombre, sGenero, sEmpleo, sEdad) {
+    this.nombre = sNombre
+    this.genero = sGenero
+    this.empleo = sEmpleo
+    this.edad = sEdad
+  }
+}
