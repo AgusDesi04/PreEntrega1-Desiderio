ed291270d5f64c6e36a9bd27c6c5130b17c77751
AgusDesi04 <agusdesi2004@gmail.com>
2024-04-24 17:10:30 -0300

    Correcciones finalizadas y agregadas nuevas funcionalidades

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// LOCAL STORAGE:
+
 let empleados = [];
 
 let empleadosLS = localStorage.getItem("empleadosCargados");
@@ -6,6 +8,9 @@ if (empleadosLS) {
 } else {
     empleados = [];
 };
+
+
+// ELEMENTOS DEL HTML:
 
 let cargarBtn = document.getElementById("botonCarga");
 let nombreInput = document.getElementById("nombre");
@@ -17,6 +22,57 @@ let buscadorInput = document.getElementById("buscador");
 let infoBtn = document.getElementById("botonInfo");
 let contenedorEmpleados = document.getElementById('contenedorEmpleados')
 let formulario = document.getElementById("formulario")
+let botonPromedio = document.getElementById("botonPromedio")
+let botonBorrarPorNombre = document.getElementById('borrarPorNombre')
+let botonBorrarTodo = document.getElementById('borrarTodo')
+
+// FUNCTIONS:
+
+function mostrarInformacionEmpleado(empleadosEncontrados) {
+
+    contenedorEmpleados.innerHTML = ''
+    empleadosEncontrados.forEach(empleado => {
+        let empleadoHTML = `
+        <div  style= "border: 2px solid black">
+            <p>Nombre: ${empleado.nombre}</p>
+            <p>Género: ${empleado.genero}</p>
+            <p>Edad: ${empleado.edad}</p>
+            <p>Cargo: ${empleado.cargo}</p>
+            <p>Salario: ${empleado.salario}</p>
+        </div>
+    `;
+        contenedorEmpleados.innerHTML += empleadoHTML
+    })
+
+}
+
+function buscarPromedio(empleados) {
+    let acumulador = 0
+    let cantidad = 0
+
+    empleados.forEach(empleado => {
+        acumulador += empleado.edad
+        cantidad++
+    })
+    return acumulador / cantidad
+
+}
+
+
+function mostrarPromedio(promedio) {
+    contenedorEmpleados.innerHTML = ''
+    contenedorEmpleados.innerHTML = `
+<div class="card text-bg-secondary mb-3" style="max-width: 18rem; margin-left: 115px; margin-top: 40px" >
+  <div class="card-header">EL PROMEDIO DE EDADES ES:</div>
+  <div class="card-body">
+    <h5 class="card-title">${promedio} AÑOS</h5>
+  </div>
+</div>
+     
+    `;
+}
+
+// EVENTOS:
 
 cargarBtn.onclick = () => {
     let nombre = nombreInput.value;
@@ -53,7 +109,7 @@ cargarBtn.onclick = () => {
                 background: "red",
             }
         }).showToast();
-    }
+    };
 
 
     console.log(empleados);
@@ -62,33 +118,52 @@ cargarBtn.onclick = () => {
 
 };
 
-function mostrarInformacionEmpleado(empleado) {
-    contenedorEmpleados.innerHTML = `
-    <p>Nombre: ${empleado.nombre}</p>
-    <p>Género: ${empleado.genero}</p>
-    <p>Edad: ${empleado.edad}</p>
-    <p>Cargo: ${empleado.cargo}</p>
-    <p>Salario: ${empleado.salario}</p>
-`;
-
-    // PROBAR CAMBIAR POR ALERTA DE SWEET ALERT
-
-}
-
 infoBtn.onclick = () => {
-    console.log(empleados)
-    let buscador = buscadorInput.value;
-    let empleado = empleados.find((el) => {
-        return el.nombre.toLowerCase() == buscador.toLowerCase()
+    let buscador = buscadorInput.value.trim().toLowerCase();
+    console.log("termino de busqueda:", buscador)
+    let empleadosEncontrados = empleados.filter((el) => {
+        return el.nombre.toLowerCase().includes(buscador);
     });
-    console.log(empleado)
+    console.log(empleadosEncontrados)
+    if (buscador != '') {
+        if (empleadosEncontrados.length > 0) {
+            contenedorEmpleados.innerHTML = ""
 
-    if (empleado != undefined) {
-        mostrarInformacionEmpleado(empleado)
+            mostrarInformacionEmpleado(empleadosEncontrados)
 
+        } else {
+            Toastify({
+                text: "No se encontro el empleado!",
+                className: "info",
+                style: {
+                    background: "red",
+                }
+            }).showToast();
+
+            contenedorEmpleados.innerHTML = ""
+        }
     } else {
         Toastify({
-            text: "No se encontro el empleado!",
+            text: "Debes rellenar el campo de busqueda para encontrar un empleado!",
+            className: "info",
+            style: {
+                background: "red",
+            }
+        }).showToast();
+
+    }
+};
+
+
+
+botonPromedio.onclick = () => {
+    let promedio = buscarPromedio(empleados)
+    if (!isNaN(promedio) && promedio !== 0) {
+        mostrarPromedio(promedio)
+        console.log(promedio)
+    } else {
+        Toastify({
+            text: "No hay empleados para mostrar un promedio!",
             className: "info",
             style: {
                 background: "red",
@@ -96,5 +171,49 @@ infoBtn.onclick = () => {
         }).showToast();
     }
 
+    promedio = 0
+
+
 };
 
+botonBorrarTodo.onclick = () => {
+    localStorage.clear();
+    location.reload();
+};
+
+
+botonBorrarPorNombre.onclick = () => {
+    let buscador = buscadorInput.value.trim().toLowerCase();
+    console.log("termino de busqueda:", buscador)
+    let empleadosABorrar = empleados.filter((el) => {
+        return el.nombre.toLowerCase().includes(buscador);
+    });
+    console.log(empleadosABorrar)
+    if (empleadosABorrar.length > 0) {
+        empleadosABorrar.forEach(empleado => {
+            let index = empleados.indexOf(empleado);
+            if (index !== -1) {
+                empleados.splice(index, 1); // Eliminar el empleado del arreglo
+            }
+        });
+
+        localStorage.setItem("empleadosCargados", JSON.stringify(empleados));
+
+        Toastify({
+            text: "Empleados eliminados correctamente.",
+            className: "info",
+            style: {
+                background: "green",
+            }
+        }).showToast();
+
+    } else {
+        Toastify({
+            text: "No hay empleados con ese nombre para borrar!",
+            className: "info",
+            style: {
+                background: "red",
+            }
+        }).showToast();
+    }
+}
